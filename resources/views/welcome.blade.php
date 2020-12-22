b27ebec1ccdeeebd6d929bb0344b4a89488763ed
agung83 <agunglaksmana908@gmail.com>
2020-12-22 08:29:14 +0700

    lah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -402,7 +402,7 @@
 
 
 
-
+    kwkwkwkwkk
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         @if (Route::has('login'))
